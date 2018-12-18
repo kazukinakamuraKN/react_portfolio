f30e48a9cad79374fb6d8d7329a247aa01d6bfe1
Kazuki Nakamura <kajirikajiri@gmail.com>
2018-12-18 19:32:57 +0900

    props test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Main extends React.Component {
+class LayerUnder extends React.Component {
   render() {
     return (
-      <div className="main">
-        Hello new branch
+      <div>
+        {this.props.value}
+      </div>
+    )
+  }
+}
+
+class Main extends React.Component {
+  renderLayerUnder(i) {
+    return (
+        <LayerUnder value={i}/>
+    )
+  }
+  render() {
+    return (
+      <div>
+        {this.renderLayerUnder(1)}
       </div>
     )
   }
